74402ce61af700e513412d79dd53297c2ed48aa9
goutarouh <gouta.gorogoro@gmail.com>
2021-04-07 23:04:21 +0900

    simple count up

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -2,26 +2,13 @@ package com.example.sample
 
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.animation.Crossfade
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import com.example.sample.ui.theme.SampleTheme
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,38 +19,26 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-enum class MyColors(val color: Color) {
-    Red(Color.Red), Green(Color.Green), Blue(Color.Blue)
-}
-
+/**
+ * jetpack composeでカウントアップする例
+ */
 @Composable
-fun CrossfadeDemo() {
-    //var currentColor by remember { mutableStateOf(MyColors.Red) }
-    var currentColor by remember { mutableStateOf<MyColors>(MyColors.Red) }
-    Column {
-        Row {
-            MyColors.values().forEach { myColors ->
-                Button(
-                    onClick = { currentColor = myColors },
-                    Modifier.weight(1f, true)
-                        .height(48.dp)
-                        .background(myColors.color)
-                        .background(myColors.color),
-                    colors = ButtonDefaults.buttonColors(backgroundColor = myColors.color)
-                ) {
-                    Text(myColors.name)
-                }
-            }
+fun Counter() {
+    //rememberでcomposeの状態を作成
+    //countの値が変更されると、適宜使用しているcomposeのみ更新する
+    var count by remember { mutableStateOf(0) }
+    Column() {
+        Button(onClick = {count++}) {
+            Text(text = "count up")
         }
-        Crossfade(targetState = currentColor, animationSpec = tween(6000)) { selectedColor ->
-            Box(modifier = Modifier.fillMaxSize().background(selectedColor.color))
-        }
+        Text(text = "$count")
     }
 }
+
 
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    CrossfadeDemo()
+    Counter()
 }
